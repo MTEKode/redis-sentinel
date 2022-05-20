094e6808baee38eb946b9b70606a92d9d73bce6c
mtoribio <marcos@farmy.ch>
2022-05-20 12:18:08 +0200

    fail over settings

--- stop.sh
+++ stop.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-LREDIS_HOME=./redis-sentinel
-kill -9 `cat $LREDIS_HOME/redis/redis1.pid`
-kill -9 `cat $LREDIS_HOME/redis/redis2.pid`
-kill -9 `cat $LREDIS_HOME/redis/redis3.pid`
+LREDIS_HOME=./redis
+kill -9 `cat $LREDIS_HOME/redis1.pid`
+kill -9 `cat $LREDIS_HOME/redis2.pid`
+kill -9 `cat $LREDIS_HOME/redis3.pid`
 
-rm $LREDIS_HOME/redis/redis1.pid
-rm $LREDIS_HOME/redis/redis2.pid
-rm $LREDIS_HOME/redis/redis3.pid
+rm $LREDIS_HOME/redis1.pid
+rm $LREDIS_HOME/redis2.pid
+rm $LREDIS_HOME/redis3.pid
 
-kill -9 `cat $LREDIS_HOME/redis/sentinel1.pid`
-kill -9 `cat $LREDIS_HOME/redis/sentinel2.pid`
-kill -9 `cat $LREDIS_HOME/redis/sentinel3.pid`
+kill -9 `cat $LREDIS_HOME/sentinel1.pid`
+kill -9 `cat $LREDIS_HOME/sentinel2.pid`
+kill -9 `cat $LREDIS_HOME/sentinel3.pid`
 
-rm $LREDIS_HOME/redis/sentinel1.pid
-rm $LREDIS_HOME/redis/sentinel2.pid
-rm $LREDIS_HOME/redis/sentinel3.pid
+rm $LREDIS_HOME/sentinel1.pid
+rm $LREDIS_HOME/sentinel2.pid
+rm $LREDIS_HOME/sentinel3.pid
 
